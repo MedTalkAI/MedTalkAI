754bd974fa14e0201e317151b90f84edc282d642
João Victor <joaovba2002@alu.ufc.br>
2024-01-10 10:37:34 -0300

    Transcription Results Done

--- src/app/Transcription/page.jsx
+++ src/app/Transcription/page.jsx
@@ -1,9 +1,23 @@
+import TranscriptionResult from "@/components/TranscriptionResult";
+
 const Transcription = () => {
-    return (
-        <div>
-            <h1>Trasncription</h1>
-        </div>
-    );
-}
- 
+  return (
+    <div>
+      <h1>Trasncription</h1>
+      <TranscriptionResult
+        text={
+          "O pacient relta uma séri de sintomas que tm impactad signitivamnt em sua qualidade de vid. Entre eles, dstaca-se a persistnt fadga, acompnhada por dores musculares e articulres frequentes."
+        }
+        isEditable={false}
+      />
+      <TranscriptionResult
+        text={
+          "O pacient relta uma séri de sintomas que tm impactad signitivamnt em sua qualidade de vid. Entre eles, dstaca-se a persistnt fadga, acompnhada por dores musculares e articulres frequentes."
+        }
+        isEditable={true}
+      />
+    </div>
+  );
+};
+
 export default Transcription;
