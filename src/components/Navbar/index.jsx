b0bc15a0c18caac26f98c70c25e16e497505ee31
yanna-torres <torres.yanna03@gmail.com>
2024-01-15 15:32:06 -0300

    update navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,12 +3,18 @@
 import "./Navbar.css";
 import insight from "../../assets/insight_mini.png";
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="left-side">
+      {/* <div className="left-side">
         <button className="hamburger-button">&#9776;</button>
+      </div> */}
+      <div className="left-side">
+        <Link href="">Transcription</Link>
+        <Link href="/history">History</Link>
+        <Link href="">Models Statistcs</Link>
       </div>
       <div className="right-side">
         <Image src={insight} alt="insight logo" />
