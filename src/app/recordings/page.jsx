7dc734e7582a3edde6593d60ed8822c0d44a4c6d
yanna-torres <torres.yanna03@gmail.com>
2024-04-20 15:50:21 -0300

    recordings table

--- src/app/recordings/page.jsx
+++ src/app/recordings/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as React from "react";
 import Navbar from "@/components/Navbar";
 import Style from "./Recordings.module.css";
 import { ToastContainer } from "react-toastify";
@@ -7,16 +8,72 @@ import "react-toastify/dist/ReactToastify.css";
 import { useEffect, useState } from "react";
 import ReactLoading from "react-loading";
 import FilterItem from "@/components/FilterItem";
+import ReactPaginate from "react-paginate";
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  TableSortLabel,
+  TableContainer,
+} from "@mui/material";
+
+const headCells = [
+  { id: "id", label: "Nº Anamnesis" },
+  { id: "anamnese", label: "Anamnesis" },
+  { id: "user", label: "User" },
+  { id: "recorded_at", label: "Date" },
+  { id: "status", label: "Status" },
+];
+
+function RTableHead({ order, orderBy, onRequestSort }) {
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            sortDirection={orderBy === headCell.id ? order : false}
+            className={Style.tableHeading}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
 
 const Recordings = () => {
+  const [order, setOrder] = useState("asc");
+  const [orderBy, setOrderBy] = useState("id");
   const [recordings, setRecordings] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [status, setStatus] = useState(['True', 'False']);
+  const [status, setStatus] = useState(["True", "False"]);
   const [users, setUsers] = useState([]);
   const [recordingsToBe, setRecordingsToBe] = useState(null);
 
   const [selectedStatus, setSelectedStatus] = useState([]);
   const [selectedUser, setSelectedUser] = useState([]);
+
+  const [pageNumber, setPageNumber] = useState(0);
+  const itemsPerPage = 10;
+  const pagesVisited = pageNumber * itemsPerPage;
+
+  const handlePageChange = ({ selected }) => {
+    setPageNumber(selected);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -57,6 +114,14 @@ const Recordings = () => {
     fetchData();
   }, []);
 
+  const formatDate = (date) => {
+    return new Date(date).toLocaleDateString("en-GB", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
+  };
+
   const handleFilterChange = (filterName, selectedOptions) => {
     if (filterName === "Transcribed") {
       setSelectedStatus(selectedOptions);
@@ -83,6 +148,46 @@ const Recordings = () => {
       selectedUser.length === 0 || selectedUser.includes(recording.user);
     return statusFilter && userFilter;
   });
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const descendingComparator = (a, b) => {
+    if (b < a) return -1;
+    if (b > a) return 1;
+    return 0;
+  };
+
+  const getComparator = (order, orderBy) => {
+    return order === "desc"
+      ? (a, b) => descendingComparator(a[orderBy], b[orderBy])
+      : (a, b) => -descendingComparator(a[orderBy], b[orderBy]);
+  };
+
+  const stableSort = (array, comparator) => {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) {
+        return order;
+      }
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  };
+
+  const sortedRecordings = stableSort(
+    filteredRecordings,
+    getComparator(order, orderBy)
+  );
+
+  const displayedAnamneses = sortedRecordings.slice(
+    pagesVisited,
+    pagesVisited + itemsPerPage
+  );
 
   return (
     <div>
@@ -155,33 +260,73 @@ const Recordings = () => {
                   )}
                 </div>
               </div>
-              <div className={Style.recordings}>
-                {filteredRecordings.map((recording) => (
-                  <div className={Style.recording}>
-                    <div className={Style.recordingInfo}>
-                      <div>
-                        <h2>{recording.id}</h2>
-                      </div>
-                    </div>
-                    <div className={Style.recordingUser}>{recording.user}</div>
-                    <div className={Style.recordingUser}>
-                      {recording.recorded_at}
-                    </div>
-                    <div className={Style.recordingUser}>
-                      {recording.anamnese_id}
-                    </div>
-                    <div
-                      className={`${Style.status} ${
-                        recording.status ? Style.done : Style.undone
-                      }`}
-                    >
-                      <span class="material-symbols-outlined">
-                        {recording.status ? "check_circle" : "cancel"}
-                      </span>
-                      {recording.status ? "Transcribed" : "Not Transcribed"}
-                    </div>
-                  </div>
-                ))}
+              <div></div>
+              <TableContainer>
+                <Table aria-labelledby="recordings" size="medium">
+                  <RTableHead
+                    order={order}
+                    orderBy={orderBy}
+                    onRequestSort={handleRequestSort}
+                  />
+                  <TableBody>
+                    {displayedAnamneses.map((recording) => (
+                      <TableRow key={recording.id}>
+                        <TableCell>{recording.anamnese_id}</TableCell>
+                        <TableCell className={Style.anamneseText}>
+                          {recording.anamnese}
+                        </TableCell>
+                        <TableCell>{recording.user}</TableCell>
+                        <TableCell>
+                          {formatDate(recording.recorded_at)}
+                        </TableCell>
+                        <TableCell>
+                          <div
+                            className={`${Style.status} ${
+                              recording.status ? Style.done : Style.undone
+                            }`}
+                          >
+                            <span class="material-symbols-outlined">
+                              {recording.status ? "check_circle" : "cancel"}
+                            </span>
+                            {recording.status
+                              ? "Transcribed"
+                              : "Not Transcribed"}
+                          </div>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <div className={Style.paginationContainer}>
+                <div className={Style.details}>
+                  Anamneses {pagesVisited} a{" "}
+                  {filteredRecordings.length > 10
+                    ? pagesVisited + 10 > filteredRecordings.length
+                      ? filteredRecordings.length
+                      : pagesVisited + 10
+                    : filteredRecordings.length}{" "}
+                  de {filteredRecordings.length}
+                </div>
+                <ReactPaginate
+                  previousLabel={
+                    <span class="material-symbols-outlined">
+                      arrow_back_ios_new
+                    </span>
+                  }
+                  nextLabel={
+                    <span class="material-symbols-outlined">
+                      arrow_forward_ios
+                    </span>
+                  }
+                  pageCount={Math.ceil(recordings.length / itemsPerPage)}
+                  onPageChange={handlePageChange}
+                  containerClassName={Style.pagination}
+                  previousLinkClassName={Style.paginationLink}
+                  nextLinkClassName={Style.paginationLink}
+                  disabledClassName={Style.paginationDisabled}
+                  activeClassName={Style.paginationActive}
+                />
               </div>
             </div>
           )}
@@ -190,4 +335,5 @@ const Recordings = () => {
     </div>
   );
 };
+
 export default Recordings;
