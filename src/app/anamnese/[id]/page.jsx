cdadbf00909594f7aa22b40c01546e046d0aa881
Yanna Torres <torres.yanna03@gmail.com>
2024-06-20 12:53:53 -0300

    refactor: update transcription page to include reference to transcriptions from other models

--- src/app/anamnese/[id]/page.jsx
+++ src/app/anamnese/[id]/page.jsx
@@ -3,27 +3,29 @@
 import { useParams } from "next/navigation";
 import Navbar from "../../../components/Navbar";
 import { useState, useEffect } from "react";
+import ReactLoading from "react-loading";
 import Style from "./Anamnese.module.css";
 import ReactAudioPlayer from "react-audio-player";
 import TranscriptionResult from "@/components/TranscriptionResult";
 import Metrics from "@/components/Metrics";
 import AnamneseChart from "@/components/AnamneseChart";
 import CheckAuthExpiration from "@/hooks/CheckAuthExpiration";
+import SimpleTable from "@/components/SimpleTable";
+import AudioStats from "@/components/AudioStats";
 
 const Anamnese = () => {
   const { id } = useParams();
   const [transcription, setTranscription] = useState(null);
-  const [corrections, setCorrections] = useState([]);
   const [audioSrc, setAudioSrc] = useState("");
-  const [dataSets, setDataSets] = useState([]);
-  const [labels, setLabels] = useState([]);
-  const [metric, setMetric] = useState("wer");
+  const [loading, setLoading] = useState(true);
+  const [transcriptionsFromOtherModels, setTranscriptionsFromOtherModels] =
+    useState([]);
 
   useEffect(() => {
     const fetchTranscription = async () => {
       try {
         const response = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/transcriptions/${id}?correction=true`,
+          `${process.env.NEXT_PUBLIC_API_URL}/transcriptions/${id}`,
           {
             method: "GET",
             headers: {
@@ -39,18 +41,7 @@ const Anamnese = () => {
         if (response.ok) {
           const data = await response.json();
           setTranscription(data);
-          setCorrections(data.corrections);
           // Extracting data for chart
-          const newLabels = data.corrections.map((correction) => {
-            return correction.date.split(" ")[0]; // Extracting data
-          });
-
-          const newDataSets = data.corrections.map((correction) => {
-            return correction;
-          });
-
-          setLabels(newLabels);
-          setDataSets(newDataSets);
         } else {
           throw new Error("Failed to fetch transcription");
         }
@@ -83,7 +74,43 @@ const Anamnese = () => {
       .catch((error) => {
         console.error("There was a problem with the fetch operation:", error);
       });
+    setLoading(false);
   }, [id]);
+
+  useEffect(() => {
+    if (transcription) {
+      const fetchTranscriptionFromOtherModels = async () => {
+        try {
+          const formData = new FormData();
+          formData.append("audio_src", transcription.audio_src);
+
+          const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/transcriptions/audio`,
+            {
+              method: "POST",
+              headers: {
+                Authorization: `Bearer ${
+                  localStorage.getItem("access_token") || ""
+                }`,
+              },
+              body: formData,
+            }
+          );
+
+          if (response.ok) {
+            const data = await response.json();
+            setTranscriptionsFromOtherModels(data);
+          } else {
+            throw new Error("Failed to fetch transcription");
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+      fetchTranscriptionFromOtherModels();
+    }
+  }, [transcription]);
 
   const formatDate = (date) => {
     return new Date(date).toLocaleDateString("en-GB", {
@@ -99,7 +126,7 @@ const Anamnese = () => {
 
   return (
     <div>
-      <Navbar />
+      <Navbar path={"/anamnesis"} />
       <CheckAuthExpiration />
       <div className={Style.anamnese}>
         <div className={Style.head}>
@@ -108,9 +135,20 @@ const Anamnese = () => {
             <span className="material-symbols-outlined">arrow_back_ios</span>
             <p>Transcription {id}</p>
           </div>
-          <h1 className={Style.title}>Anamnese {id}</h1>
+          <h1 className={Style.title}>Transcription {id}</h1>
         </div>
-        {transcription && (
+        {loading && (
+          <div className={Style.loading}>
+            <ReactLoading
+              type="spinningBubbles"
+              color="#001D3B"
+              height={"100px"}
+              width={"100px"}
+            />
+            <p>Loading...</p>
+          </div>
+        )}
+        {transcription && !loading && (
           <>
             <div className={Style.audio}>
               <div className={Style.metadata_cont}>
@@ -141,48 +179,49 @@ const Anamnese = () => {
               </div>
               <div className={Style.controls}>
                 <ReactAudioPlayer src={audioSrc} controls />
+                <span>{transcription.audio_src}</span>
               </div>
             </div>
             <div className={Style.content}>
               <div className={Style.texts}>
-                <TranscriptionResult
-                  isEditable={false}
-                  text={transcription?.transcription}
-                />
-                <TranscriptionResult
-                  isEditable={true}
-                  text={transcription?.latest_correction}
-                />
-              </div>
-              <div className={Style.metrics}>
-                <Metrics
-                  bleu={parseFloat(transcription.bleu)}
-                  cosine={parseFloat(transcription.cosine)}
-                  kappa={parseFloat(transcription.kappa)}
-                  transcription={transcription.transcription}
-                  wer={parseFloat(transcription.wer)}
-                />
-                <div className={Style.selection}>
-                  <h3>Historic Metrics: </h3>
-                  <select
-                    onChange={(e) => {
-                      setMetric(e.target.value);
-                    }}
-                    value={metric}
-                  >
-                    <option value={"wer"}>WER</option>
-                    <option value={"bleu"}>BLEU</option>
-                    <option value={"cosine"}>COSINE SIMILARITY</option>
-                  </select>
+                <div className={Style.transcription}>
+                  <h3>Model Transcript</h3>
+                  <div className={Style.model}>
+                    {transcription?.transcription}
+                  </div>
                 </div>
-                <AnamneseChart
-                  dataSets={dataSets}
-                  labels={labels}
-                  metric={metric}
-                />
+                <div className={Style.transcription}>
+                  <h3>Correct Transcript</h3>
+                  <div className={Style.correct}>
+                    {transcription?.latest_correction}
+                  </div>
+                </div>
+              </div>
+              <div className={Style.evaluation}>
+                <div className={Style.metrics}>
+                  <Metrics
+                    bleu={parseFloat(transcription.bleu)}
+                    cosine={parseFloat(transcription.cosine)}
+                    transcription={transcription.transcription}
+                    wer={parseFloat(transcription.wer)}
+                  />
+                  <AnamneseChart dataSets={transcriptionsFromOtherModels} />
+                </div>
+                <div className={Style.other}>
+                  <SimpleTable
+                    data={transcriptionsFromOtherModels}
+                    current={id}
+                  />
+                  <AudioStats data={transcriptionsFromOtherModels} />
+                </div>
               </div>
             </div>
           </>
+        )}
+        {!transcription && !loading && (
+          <div className={Style.error}>
+            <p>Failed to fetch transcription</p>
+          </div>
         )}
       </div>
     </div>
